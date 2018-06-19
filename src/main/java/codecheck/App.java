a209f6b1df495fd55562d9f44be656f1e842e6ee
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-19 11:07:31 +0900

    4

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,7 @@ import java.lang.*;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
+			String output = String.format("argv[%s]: %s", i, args[i]);
 
 			String input = args[i];
 
