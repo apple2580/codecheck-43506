dbca8d50c47c5001f89953778bbde0014d757127
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-19 11:06:14 +0900

    3

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,32 +5,33 @@ import java.lang.*;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
 
 			String input = args[i];
 
 			try {
+
 				int num = Integer.parseInt(input);
 
 				if (num > 1000) {
 					output = "invalid";
 				} else if (input.contains("3")) {
-					if num%3 == 0) {
+					if (num%3 == 0) {
 						output = "dumb";
 					} else {
 						output = "stupid";
 					}
 				} else if (num%3 == 0) {
 						output = "idiot";
-					}
 				} else {
 					output = "invalid";
+				}
 
 			} catch (NumberFormatException e) {
 				output = "invalid";
 			}
 
 			System.out.println(output);
+
 		}
 	}
 }
