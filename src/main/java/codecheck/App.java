219df528d41fef48132c668ec8f47d0bdcba2899
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-19 10:41:40 +0900

    1

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,9 +1,29 @@
 package codecheck;
 
+import java.lang.*;
+
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
+
+			String input = args[i];
+
+			try {
+				int num = Integer.parseInt(input);
+
+				if (num > 1000) {
+					output = "invalid";
+				} else {
+					if (num%3 == 0) {
+						output = "idiot";
+					}
+				}
+
+			} catch (NumberFormatException e) {
+				output = "invalid";
+			}
+
 			System.out.println(output);
 		}
 	}
